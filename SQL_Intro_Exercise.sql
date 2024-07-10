7e0189a5b32e347a7d4eb692a8216818d82afaf5
Damien Young <109642054+dyoung1006@users.noreply.github.com>
2024-07-10 08:52:51 -0700

    updated intro

--- SQL_Intro_Exercise.sql
+++ SQL_Intro_Exercise.sql
@@ -1,40 +1,40 @@
 USE bestbuy
 
 -- find all products 
-SELECT * FROM Prdocts
+SELECT * FROM Products;
  
  -- find all products that cost $1400
- SELECT * FROM Products WHERE Price = 1400.00
+ SELECT * FROM Products WHERE Price = 1400.00;
  
 -- find all products that cost $11.99 or $13.99
- SELECT * FROM Products WHERE Price = 11.99 OR PRICE = 13.99 ORDER BY 3
+ SELECT * FROM Products WHERE Price = 11.99 OR PRICE = 13.99 ORDER BY 3;
  
 -- find all products that do NOT cost 11.99 - using NOT
- SELECT * FROM Products WHERE NOT Price = 11.99 ORDER BY 3
+ SELECT * FROM Products WHERE NOT Price = 11.99 ORDER BY 3;
  
 -- find  all products and sort them by price from greatest to least
  SELECT * FROM Products ORDER BY 3 DESC -- 3 is price
 
 -- find all employees who don't have a middle initial
- SELECT * FROM employees WHERE MiddleInitial IS NULL
+ SELECT * FROM employees WHERE MiddleInitial IS NULL;
   
 -- find distinct product prices
-SELECT DISTINCT Price FROM products
+SELECT DISTINCT Price FROM products;
 
 -- find all employees whose first name starts with the letter ‘j’
- SELECT * FROM employees WHERE FirstName like 'j%'
+ SELECT * FROM employees WHERE FirstName like 'j%';
 
 -- find all Macbooks 
-SELECT * FROM products wHERE Name like 'mac%'
+SELECT * FROM products WHERE Name like 'mac%';
  
 -- find all products that are on sale
- SELECT * FROM products wHERE OnSale = 1
+ SELECT * FROM products WHERE OnSale = 1;
  
 -- find the average price of all products 
- SELECT AVG(Price) FROM products
+ SELECT AVG(Price) FROM products;
  
 -- find all Geek Squad employees who don't have a middle initial 
- SELECT * FROM Employees WHERE title like 'geek%' AND MiddleInitial IS NULL
+ SELECT * FROM Employees WHERE title like 'geek%' AND MiddleInitial IS NULL;
  
 -- find all products from the products table whose stock level is in the range  -- of 500 to 1200. Order by Price from least to greatest. **Use the between keyword** 
-SELECT * FROM products WHERE StockLevel BETWEEN 500 AND 1200 ORDER BY Price
+SELECT * FROM products WHERE StockLevel BETWEEN 500 AND 1200 ORDER BY Price;
